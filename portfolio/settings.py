f4a7ca3f3c4ebb38d00a049b3eb43958c8718a53
sanjay chudali <poudelsanjay93@gmail.com>
2025-12-18 21:17:43 +0545

    Refactor database configuration for Django settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,18 +76,20 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-database_config = dj_database_url.parse(
-    os.environ.get("DATABASE_URL")
-)
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
 
 # Set default port if not provided
 if not database_config.get('PORT'):
     database_config['PORT'] = '5432'
 
-DATABASES = {
-   'default': database_config
-}
-# Password validation
+
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
