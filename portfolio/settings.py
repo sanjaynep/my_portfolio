9b0296633be2a117d9aa11cead65382087b68722
sanjay chudali <poudelsanjay93@gmail.com>
2025-12-19 15:31:02 +0545

    Remove email configuration from settings.py
    Removed email configuration settings from settings.py.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -136,15 +136,5 @@ STORAGES = {
 }
 
 
-# email configuration
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = os.environ.get("EMAIL_NAME")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
